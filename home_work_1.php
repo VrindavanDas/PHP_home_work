d654d61809723399a4b6d97da44c1b95d5b270f2
BukaVrindavan_das <noblevic@gmail.com>
2019-01-02 22:01:49 +0200

    add third task

--- home_work_1.php
+++ home_work_1.php
@@ -3,6 +3,7 @@
 /*----------------Test data----------------*/
 $myArray = [12, 5, 78, 4, -32, 1, 34, 45, 0, -12, 234, 45];
 $maxLimit = 32;
+$fibonacciNumber = 20;
 
 /*----------------Tasks resolve----------------*/
 function task0MaxElement($testArr) {
@@ -29,17 +30,36 @@ function task1PositiveNumbers($testArr, $limit) {
             ($testArr[$i] < $limit)
         ){
 
-            array_push($resultArr, $testArr[$i]);
+            /*array_push($resultArr, $testArr[$i]);*/
+            $resultArr[] = $testArr[$i];
         }
     }
 
-    /*echo $resultArr;*/
     var_dump($resultArr);
 }
 
-/*----------------Tasks test----------------*/
-task0MaxElement($myArray);
-task1PositiveNumbers($myArray, $maxLimit);
+function task2FibonacciSequence($n) {
+    $fib = [0, 1];
 
+    for ($i=1; $i < ($n - 1); $i++) {
+        $fib[] = $fib[$i] + $fib[$i - 1];
+    }
+
+    var_dump($fib);
+}
+
+/*----------------Testing the tasks----------------*/
+echo "task 0 result:";
+echo "\n";
+task0MaxElement($myArray);
+echo "\n\n";
+echo "task 1 result:";
+echo "\n";
+task1PositiveNumbers($myArray, $maxLimit);
+echo "\n\n";
+echo "task 2 result:";
+echo "\n";
+task2FibonacciSequence($fibonacciNumber);
+echo "\n\n";
 
 ?>
