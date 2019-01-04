8da9a28d681526e291f5df0b8bef5b898ce60116
BukaVrindavan_das <noblevic@gmail.com>
2019-01-04 21:53:04 +0200

    add tasks 4 and 5

--- home_work_1.php
+++ home_work_1.php
@@ -5,6 +5,7 @@ $myArray = [12, 5, 78, 4, -32, 1, 34, 45, 0, -12, 234, 45];
 $maxLimit = 32;
 $fibonacciNumber = 20;
 $replaceNumber = 40;
+$searchValue = 5;
 
 /*----------------Tasks resolve----------------*/
 function task0MaxElement($testArr) {
@@ -71,22 +72,67 @@ function task3ReplaceEl ($arr , $value) {
 
 }
 
+function sumArray ($array) {
+    $result = 0;
+
+    for ($i=0; $i<count($array); $i++) {
+
+        $result += $array[$i]; /*$result = $result + $array[$i];*/
+
+    }
+
+    return $result;
+
+}
+
+function inArray ($array, $value){
+    $result = false;
+
+    for ($i=0; $i<count($array); $i++) {
+        if ($array[$i]===$value) {
+            $result = true;
+
+        }
+
+    }
+    return $result;
+}
+
 /*----------------Testing the tasks----------------*/
-echo "task 0 result:";
-echo "\n";
+echo "task 0 result: \n";
 task0MaxElement($myArray);
 echo "\n\n";
-echo "task 1 result:";
-echo "\n";
+
+
+echo "task 1 result: \n";
 task1PositiveNumbers($myArray, $maxLimit);
 echo "\n\n";
-echo "task 2 result:";
-echo "\n";
+
+
+echo "task 2 result: \n";
 task2FibonacciSequence($fibonacciNumber);
 echo "\n\n";
-echo "task 3 result:";
+
+
+echo "task 3 result: \n";
 task3ReplaceEl($myArray, $replaceNumber);
 echo "\n\n";
+
+
+$arraySum = sumArray($myArray);
+echo "task 4 result: \n $arraySum \n\n";
+
+
+$arrayIn = inArray($myArray, $searchValue);
+
+if($arrayIn) /*($arrayIn === true)*/ {
+    echo "task 5 result: \n true \n\n";
+} else {
+    echo "task 5 result: \n false \n\n";
+}
+
+
+
 
 ?>
 
